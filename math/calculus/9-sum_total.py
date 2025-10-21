042c2a4e05b28a23b4b993b2d134ecc7cfcbb2b2
kloekam <keg7341@gmail.com>
2025-10-21 11:25:23 +0000

    Fixed condition on task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """A function that calculates the sum of squares"""
-    if type(n) is not int:
+    if type(n) is not int or n < 1:
         return None
     return n * (n + 1) * (2 * n + 1) // 6
