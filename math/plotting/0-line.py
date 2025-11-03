8fbe7bbdd044462451da6d487736627e916fae03
kloekam <keg7341@gmail.com>
2025-11-03 23:56:01 +0100

    Fixed comments in task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,11 +8,13 @@ import matplotlib.pyplot as plt
 
 
 def line():
-
+    """A function that plots a cubic function as a line graph"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
+    # Set the line color to red
     plt.plot(y, color="red")
+    # Set the x-axis limit
     plt.xlim(0, 10)
 
     plt.show()
