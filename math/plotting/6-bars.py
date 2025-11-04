53a27c36ad40c19832c3bb536b2ea11d6877d0f8
kloekam <keg7341@gmail.com>
2025-11-04 15:37:36 +0100

    Added task 6 solution

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""A script that plots a stacked bar chart of fruit per person"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """A function that plots a stacked bar chart of fruit per person"""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    people = ["Farrah", "Fred", "Felicia"]
+    x = np.arange(len(people))
+
+    apples = fruit[0]
+    bananas = fruit[1]
+    oranges = fruit[2]
+    peaches = fruit[3]
+
+    # Plot stacked bars
+    plt.bar(x, apples, color="red", width=0.5, label="apples")
+    plt.bar(x, bananas, bottom=apples, color="yellow",
+            width=0.5, label="bananas")
+    plt.bar(x, oranges, bottom=apples+bananas, color="#ff8000",
+            width=0.5, label="oranges")
+    plt.bar(x, peaches, bottom=apples+bananas+oranges, color="#ffe5b4",
+            width=0.5, label="peaches")
+
+    plt.xticks(x, people)
+    plt.ylim(0, 80)
+    plt.yticks(np.arange(0, 80, step=10))
+
+    # Set title and label
+    plt.ylabel("Quantity of Fruit")
+    plt.title("Number of Fruit per Person")
+
+    plt.legend()
+    plt.show()
