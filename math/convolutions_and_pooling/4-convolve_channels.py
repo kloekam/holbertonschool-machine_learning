8f37ddad5a19d53089d8fb2e9ce29f117cabad0b
kloekam <keg7341@gmail.com>
2026-03-04 15:10:40 +0100

    Fixed solutions for tasks 4 and 5

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -15,30 +15,28 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     kh, kw, _ = kernel.shape
     sh, sw = stride
 
-    if padding == 'valid':
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
         ph, pw = 0, 0
-
-    elif padding == 'same':
-        ph = (kh - 1) // 2
-        pw = (kw - 1) // 2
-
     else:
         ph, pw = padding
 
+    padded = np.pad(images, (
+        (0, 0),
+        (ph, ph),
+        (pw, pw),
+        (0, 0)), mode='constant')
+
     out_h = (h + 2 * ph - kh) // sh + 1
     out_w = (w + 2 * pw - kw) // sw + 1
-
-    padded = np.pad(images,
-                    ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                    mode='constant')
 
     output = np.zeros((m, out_h, out_w))
 
     for i in range(out_h):
         for j in range(out_w):
-            i_start = i * sh
-            j_start = j * sw
-            region = padded[:, i_start:i_start + kh, j_start:j_start + kw, :]
+            region = padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
             output[:, i, j] = np.sum(region * kernel, axis=(1, 2, 3))
 
     return output
--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -15,28 +15,28 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     kh, kw, _, nc = kernels.shape
     sh, sw = stride
 
-    if padding == 'valid':
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
         ph, pw = 0, 0
-    elif padding == 'same':
-        ph = (kh - 1) // 2
-        pw = (kw - 1) // 2
     else:
         ph, pw = padding
 
-    out_h = 1 + (h + 2 * ph - kh) // sh
-    out_w = 1 + (w + 2 * pw - kw) // sw
-
-    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw),
+    padded = np.pad(images, ((0, 0),
+                             (ph, ph),
+                             (pw, pw),
                              (0, 0)), mode='constant')
+
+    out_h = (h + 2 * ph - kh) // sh + 1
+    out_w = (w + 2 * pw - kw) // sw + 1
 
     output = np.zeros((m, out_h, out_w, nc))
 
-    for i in range(out_h):
-        for j in range(out_w):
-            for k in range(nc):
-                i_start = i * sh
-                j_start = j * sw
-                region = padded[:, i_start:i_start+kh, j_start:j_start+kw, :]
+    for k in range(nc):
+        for i in range(out_h):
+            for j in range(out_w):
+                region = padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
                 output[:, i, j, k] = np.sum(region * kernels[:, :, :, k],
                                             axis=(1, 2, 3))
 
