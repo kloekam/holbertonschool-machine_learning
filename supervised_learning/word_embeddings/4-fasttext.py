fc71e55565e799092254213d6df6ee0cdefc4f96
Kloe K <115482690+kloekam@users.noreply.github.com>
2026-06-26 15:50:27 +0200

    Add FastText model training module

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""FastText model training module."""
+from gensim.models import FastText
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Create, build, and train a gensim FastText model."""
+    sg = 0 if cbow else 1
+    model = FastText(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        negative=negative,
+        window=window,
+        sg=sg,
+        epochs=epochs,
+        seed=seed,
+        workers=workers
+    )
+    return model
