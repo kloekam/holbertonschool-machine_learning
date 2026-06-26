eda92d3e72577f68bfc8284cf1e1957475858f07
Kloe K <115482690+kloekam@users.noreply.github.com>
2026-06-26 17:07:49 +0200

    Refactor word2vec_model to build vocab and train

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -11,7 +11,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     """
     sg = 0 if cbow else 1
     model = gensim.models.Word2Vec(
-        sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
@@ -20,5 +19,11 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         epochs=epochs,
         seed=seed,
         workers=workers
+    )
+    model.build_vocab(sentences)
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=model.epochs
     )
     return model
