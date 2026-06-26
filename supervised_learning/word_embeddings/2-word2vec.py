dc06910d65b58090bfab8661d2c7eb624ea6a6b9
Kloe K <115482690+kloekam@users.noreply.github.com>
2026-06-26 16:40:21 +0200

    Update word2vec_model to include vocabulary building
    Refactor word2vec_model function to build vocabulary before training.

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 """Word2Vec model training module."""
-import os
-import sys
-if 'PYTHONHASHSEED' not in os.environ:
-    os.environ['PYTHONHASHSEED'] = '0'
-    os.execv(sys.executable, [sys.executable] + sys.argv)
 import gensim
 
 
@@ -12,12 +7,11 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
     Create, build, and train a gensim Word2Vec model.
-    Returns the trained model
+    Returns the trained model.
     """
 
     sg = 0 if cbow else 1
     model = gensim.models.Word2Vec(
-        sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
@@ -27,5 +21,10 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         seed=seed,
         workers=workers
     )
+    model.build_vocab(sentences)
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=model.epochs
+
     return model
-  
