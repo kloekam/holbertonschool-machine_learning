bee59255bd80c625eab081fc3125d24ff051a045
Kloe K <115482690+kloekam@users.noreply.github.com>
2026-06-26 17:15:13 +0200

    Update 4-fasttext.py

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """FastText model training module."""
-from gensim.models import FastText
+import gensim
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                    window=5, cbow=True, epochs=5, seed=0, workers=1):
     """Create, build, and train a gensim FastText model."""
     sg = 0 if cbow else 1
-    model = FastText(
+    model = gensim.models.FastText(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
